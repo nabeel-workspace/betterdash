c45b4481ee06564096fb501c01a2655c4093a127
NabeelWorkspace <115373801+nabeel-workspace@users.noreply.github.com>
2025-12-23 00:16:26 +0500

    Add and configure prettier-plugin-sort-imports
    Added @ianvs/prettier-plugin-sort-imports to dependencies and updated prettier.config.js with import sorting rules and related plugin options to enforce consistent import order across the codebase.

--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,37 @@
 
 /** @type {import('prettier').Config} */
 const config = {
+  endOfLine: 'lf',
   semi: false,
   singleQuote: true,
-  trailingComma: "all",
-};
+  tabWidth: 2,
+  trailingComma: 'all',
+  importOrder: [
+    '^(react/(.*)$)|^(react$)',
+    '^(next/(.*)$)|^(next$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^types$',
+    '^@/types/(.*)$',
+    '^@/config/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/components/ui/(.*)$',
+    '^@/components/(.*)$',
+    '^@/registry/(.*)$',
+    '^@/styles/(.*)$',
+    '^@/app/(.*)$',
+    '^@/www/(.*)$',
+    '',
+    '^[./]',
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderBuiltinModulesToTop: true,
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderMergeDuplicateImports: true,
+  importOrderCombineTypeAndValueImports: true,
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+}
 
-export default config;
+export default config
